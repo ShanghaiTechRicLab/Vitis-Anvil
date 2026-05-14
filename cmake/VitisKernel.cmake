acbf242959f596da36fe276df829dc06fa2996eb
dengqi <dengqi935@gmail.com>
2026-05-14 21:27:20 +0800

    build(phase2): add cosim support to add_accel_kernel

--- cmake/VitisKernel.cmake
+++ cmake/VitisKernel.cmake
@@ -11,7 +11,7 @@ endfunction()
 
 function(add_accel_kernel)
   set(options)
-  set(one_value_args NAME TOP CLOCK_HZ PLATFORM_KIND)
+  set(one_value_args NAME TOP CLOCK_HZ PLATFORM_KIND TESTBENCH)
   set(multi_value_args SOURCES)
   cmake_parse_arguments(AK "${options}" "${one_value_args}" "${multi_value_args}" ${ARGN})
 
@@ -82,8 +82,27 @@ function(add_accel_kernel)
     list(APPEND _ak_abs_sources "${_ak_abs_src}")
   endforeach()
 
+  if(AK_TESTBENCH)
+    if(IS_ABSOLUTE "${AK_TESTBENCH}")
+      set(_ak_abs_testbench "${AK_TESTBENCH}")
+    else()
+      get_filename_component(_ak_abs_testbench "${AK_TESTBENCH}" ABSOLUTE
+                             BASE_DIR "${CMAKE_CURRENT_SOURCE_DIR}")
+    endif()
+
+    if(AK_PLATFORM_KIND STREQUAL "alveo_u250")
+      set(_ak_cosim_part "xcu250-figd2104-2L-e")
+    else()
+      message(FATAL_ERROR
+        "add_accel_kernel(${AK_NAME}): TESTBENCH cosim does not yet support "
+        "PLATFORM_KIND='${AK_PLATFORM_KIND}'. Add a Vitis part mapping before "
+        "enabling cosim for this platform kind.")
+    endif()
+  endif()
+
   set(_ak_work_dir "${CMAKE_CURRENT_BINARY_DIR}/${AK_NAME}_hls")
   set(_ak_cfg "${_ak_work_dir}/hls.cfg")
+  set(_ak_cosim_cfg "${_ak_work_dir}/cosim.cfg")
   set(_ak_xo "${_ak_work_dir}/${AK_NAME}.xo")
   set(_ak_csynth_xml "${_ak_work_dir}/hls/syn/report/${AK_TOP}_csynth.xml")
 
@@ -96,6 +115,9 @@ function(add_accel_kernel)
   foreach(_ak_abs_src IN LISTS _ak_abs_sources)
     _accel_kernel_reject_space_path("${AK_NAME}" "kernel source path" "${_ak_abs_src}")
   endforeach()
+  if(AK_TESTBENCH)
+    _accel_kernel_reject_space_path("${AK_NAME}" "testbench path" "${_ak_abs_testbench}")
+  endif()
   _accel_kernel_reject_space_path("${AK_NAME}" "kernel work directory" "${_ak_work_dir}")
   _accel_kernel_reject_space_path("${AK_NAME}" "kernel artifact path" "${_ak_xo}")
 
@@ -108,6 +130,8 @@ function(add_accel_kernel)
 
   set(_ak_cflags
     "-std=${ACCEL_HLS_STD} -DHLSLIB_SYNTHESIS -I${PROJECT_SOURCE_DIR}/include -I${CMAKE_BINARY_DIR}/generated -I${PROJECT_SOURCE_DIR}/third_party/hlslib/include")
+  set(_ak_tb_cflags
+    "-std=${ACCEL_HLS_STD} -I${PROJECT_SOURCE_DIR}/include -I${CMAKE_BINARY_DIR}/generated -I${PROJECT_SOURCE_DIR}/third_party/hlslib/include")
 
   # Vitis 2024.2 HLS compile mode accepts platform/frequency but not --target;
   # ACCEL_VITIS_TARGET is retained as target metadata for future xclbin/link work.
@@ -122,6 +146,21 @@ function(add_accel_kernel)
     "flow_target=vitis\n"
     "package.output.format=xo\n"
     "package.output.file=${_ak_xo}\n")
+
+  if(AK_TESTBENCH)
+    # Vitis 2024.2 cosim rejects the compile config's top-level platform= key;
+    # keep cosim on a separate part-based config.
+    file(WRITE "${_ak_cosim_cfg}"
+      "part=${_ak_cosim_part}\n"
+      "freqhz=${AK_CLOCK_HZ}\n"
+      "\n"
+      "[hls]\n"
+      "syn.top=${AK_TOP}\n"
+      "syn.cflags=${_ak_cflags}\n"
+      "${_ak_syn_files}"
+      "tb.file=${_ak_abs_testbench}\n"
+      "tb.file_cflags=${_ak_abs_testbench},${_ak_tb_cflags}\n")
+  endif()
 
   add_custom_command(
     OUTPUT "${_ak_xo}" "${_ak_csynth_xml}"
@@ -148,6 +187,34 @@ function(add_accel_kernel)
     ACCEL_KERNEL_CLOCK_HZ "${AK_CLOCK_HZ}"
     ACCEL_KERNEL_VITIS_TARGET "${ACCEL_VITIS_TARGET}")
 
+  if(AK_TESTBENCH)
+    set(_ak_cosim_stamp "${_ak_work_dir}/.cosim.stamp")
+    add_custom_command(
+      OUTPUT "${_ak_cosim_stamp}"
+      COMMAND "${CMAKE_COMMAND}" -E make_directory "${_ak_work_dir}"
+      COMMAND "${VITIS_RUN_EXECUTABLE}"
+              --cosim
+              --config "${_ak_cosim_cfg}"
+              --work_dir "${_ak_work_dir}"
+      COMMAND "${CMAKE_COMMAND}" -E touch "${_ak_cosim_stamp}"
+      DEPENDS "${_ak_xo}" "${_ak_abs_testbench}" "${_ak_cosim_cfg}" ${_ak_abs_sources}
+      COMMENT "Running Vitis HLS cosim for ${AK_NAME}"
+      VERBATIM
+      USES_TERMINAL)
+
+    add_custom_target("${AK_NAME}_cosim"
+      DEPENDS "${_ak_cosim_stamp}")
+
+    set_target_properties("${AK_NAME}_cosim" PROPERTIES
+      ACCEL_KERNEL_COSIM_STAMP "${_ak_cosim_stamp}"
+      ACCEL_KERNEL_COSIM_CONFIG "${_ak_cosim_cfg}"
+      ACCEL_KERNEL_TESTBENCH "${_ak_abs_testbench}"
+      ACCEL_KERNEL_WORK_DIR "${_ak_work_dir}"
+      ACCEL_KERNEL_TOP "${AK_TOP}"
+      ACCEL_KERNEL_PLATFORM_KIND "${AK_PLATFORM_KIND}"
+      ACCEL_KERNEL_CLOCK_HZ "${AK_CLOCK_HZ}")
+  endif()
+
   if(ACCEL_BUILD_TESTS)
     if(NOT Python3_EXECUTABLE)
       message(FATAL_ERROR
@@ -171,6 +238,17 @@ function(add_accel_kernel)
     set_tests_properties("${AK_NAME}_csynth_check" PROPERTIES
       LABELS csynth
       FIXTURES_REQUIRED "${AK_NAME}_csynth_fixture")
+
+    if(AK_TESTBENCH)
+      add_test(
+        NAME "${AK_NAME}_cosim_vs_gold"
+        COMMAND "${CMAKE_COMMAND}"
+                --build "${CMAKE_BINARY_DIR}"
+                --target "${AK_NAME}_cosim")
+      set_tests_properties("${AK_NAME}_cosim_vs_gold" PROPERTIES
+        LABELS cosim
+        TIMEOUT 600)
+    endif()
   endif()
 endfunction()
 
